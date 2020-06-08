2a676741f9685fe188321ff196b6f764f0da2ac0
Danilo Rodrigues Catapan <dcatapan@gmail.com>
2020-06-08 15:39:21 -0300

    feat: add module players

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { PlayersModule } from './players/players.module';
 
 @Module({
-  imports: [],
+  imports: [PlayersModule],
   controllers: [],
   providers: [],
 })
