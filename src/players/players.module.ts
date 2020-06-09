7e657f972658e1e72284a4f84d62d8c4946bbd6a
Danilo Rodrigues Catapan <dcatapan@gmail.com>
2020-06-09 16:04:40 -0300

    feat: configure mongoose in player module

--- src/players/players.module.ts
+++ src/players/players.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common'
 import { PlayersController } from './players.controller'
 import { PlayersService } from './players.service'
+import { MongooseModule } from '@nestjs/mongoose'
+import { PlayerSchema } from './interfaces/player.schema'
 
 @Module({
+  imports: [MongooseModule.forFeature([{ name: 'Player', schema: PlayerSchema }])],
   controllers: [PlayersController],
   providers: [PlayersService]
 })
