59897bbae2145c630273a4dd559fda2c94c10b26
Danilo Rodrigues Catapan <dcatapan@gmail.com>
2020-06-09 17:04:47 -0300

    refactor: add bad request to PlayersParametersValidationPipe

--- src/players/pipes/player-parameters-validation.pipes.ts
+++ src/players/pipes/player-parameters-validation.pipes.ts
@@ -1,8 +1,10 @@
-import { PipeTransform, ArgumentMetadata } from "@nestjs/common";
+import { PipeTransform, ArgumentMetadata, BadRequestException } from "@nestjs/common";
 
 export class PlayersParametersValidationPipe implements PipeTransform {
   transform (value: any, metaData: ArgumentMetadata) {
-    console.log(`value: ${value} - metaData: ${metaData} `)
+    if (!value) {
+      throw new BadRequestException(`The parameter ${metaData.data} can't be empty`)
+    }
     return value
   }
 }
