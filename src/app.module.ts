f2f43eee81944abf37070f5421593631fca11f8a
Danilo Rodrigues Catapan <dcatapan@gmail.com>
2020-06-09 15:11:03 -0300

    chore: add mongoose module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common'
+import { MongooseModule } from '@nestjs/mongoose'
 import { PlayersModule } from './players/players.module'
 
 @Module({
-  imports: [PlayersModule],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://admin:admin123@cluster0-wyrd1.mongodb.net/smartranking?retryWrites=true&w=majority',
+      { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: false }),
+    PlayersModule],
   controllers: [],
   providers: [],
 })
