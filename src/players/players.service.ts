a1efb26419e895fc85e7d0c763e1c73bce4efc8f
Danilo Rodrigues Catapan <dcatapan@gmail.com>
2020-06-09 16:06:16 -0300

    refactor: create player

--- src/players/players.service.ts
+++ src/players/players.service.ts
@@ -2,18 +2,22 @@ import { Injectable, Logger, NotFoundException } from '@nestjs/common'
 import { CreatePlayerDto } from './dtos/create-player.dto'
 import { Player } from './interfaces/player.interface'
 import * as faker from 'faker'
+import { InjectModel } from '@nestjs/mongoose'
+import { Model } from 'mongoose'
 
 @Injectable()
 export class PlayersService {
 
   private players: Player[] = []
 
+  constructor (@InjectModel('Player') private playerModel: Model<Player>) {}
+
   private readonly logger = new Logger(PlayersService.name)
 
   async save(createPlayerDto: CreatePlayerDto): Promise<void> {
     const { email } = createPlayerDto
 
-    const playerFound = await this.players.find(player => player.email === email)
+    const playerFound = await this.playerModel.findOne({ email }).exec()
 
     if (playerFound) {
       await this.update(playerFound, createPlayerDto)
@@ -43,18 +47,9 @@ export class PlayersService {
     return playerFound
   }
 
-  private create(createPlayerDto: CreatePlayerDto): void {
-    const { name, cellphone, email } = createPlayerDto
-    const player: Player = {
-      _id: faker.random.uuid(),
-      name,
-      email,
-      cellphone,
-      ranking: faker.random.alphaNumeric(),
-      positionRanking: faker.random.number(),
-      urlPhotoPlayer: faker.image.imageUrl()
-    }
-    this.players.push(player)
+  private async create(createPlayerDto: CreatePlayerDto): Promise<Player> {
+    const player = new this.playerModel(createPlayerDto)
+    return await player.save()
   }
 
   private update(playerFound: Player, createPlayerDto: CreatePlayerDto): void {
