2b56660b3e6ea6a4a43db57ea14d920e08185f72
Danilo Rodrigues Catapan <dcatapan@gmail.com>
2020-06-11 17:17:40 -0300

    feat: add save challenge

--- src/challenge/challenge.controller.ts
+++ src/challenge/challenge.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, UsePipes, ValidationPipe, Body } from '@nestjs/common'
+import { CreateChallengeDto } from './dtos/create-challenge.dto'
+import { Challenge } from './interface'
+import { ChallengeService } from './challenge.service'
 
-@Controller('challenge')
-export class ChallengeController {}
+@Controller('api/v1/challenge')
+export class ChallengeController {
+  constructor(private readonly challengeService: ChallengeService) {}
+
+  @Post()
+  @UsePipes(ValidationPipe)
+  async save(@Body() createChallengeDto: CreateChallengeDto): Promise<Challenge> {
+    return await this.challengeService.save(createChallengeDto)
+  }
+}
--- src/challenge/challenge.service.ts
+++ src/challenge/challenge.service.ts
@@ -1,4 +1,46 @@
-import { Injectable } from '@nestjs/common';
+import { Challenge } from './interface'
+import { CreateChallengeDto } from './dtos/create-challenge.dto'
+import { ChallengeStatus } from './enums/challenge-status.enum'
+import { PlayersService } from 'src/players'
+import { CategoriesService } from 'src/categories'
+import { InjectModel } from '@nestjs/mongoose'
+import { Injectable, BadRequestException } from '@nestjs/common'
+import { Model } from 'mongoose'
 
 @Injectable()
-export class ChallengeService {}
+export class ChallengeService {
+  constructor(
+    @InjectModel('Challenge') private readonly challengeModel: Model<Challenge>,
+    private readonly playersService: PlayersService,
+    private readonly categoriesService: CategoriesService
+  ) {}
+
+  async save(createChallengeDto: CreateChallengeDto): Promise<Challenge> {
+    const players = await this.playersService.get()
+    createChallengeDto.players.map(playerDto => {
+      const filter = players.filter(player => player._id == playerDto._id)
+      if (filter.length === 0) {
+        throw new BadRequestException(`O id ${playerDto._id} is not a player`)
+      }
+    })
+
+    const challenger = await createChallengeDto.players.filter(player => player._id == createChallengeDto.challenger)
+
+    if (challenger.length == 0) {
+      throw new BadRequestException(`The challenger must be a match player`)
+    }
+
+    const category = await this.categoriesService.getByIdPlayer(createChallengeDto.challenger)
+
+    if (!category) {
+      throw new BadRequestException(`The challenger must be registered in a category`)
+    }
+
+    const challenge = new this.challengeModel(createChallengeDto)
+    challenge.category = category.category
+    challenge.dateHoraRequest = new Date()
+
+    challenge.status = ChallengeStatus.PENDING
+    return await challenge.save()
+  }
+}
