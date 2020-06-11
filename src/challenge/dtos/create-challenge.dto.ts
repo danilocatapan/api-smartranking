669b813d7388a35f7cba11d6fe96804ab4a21333
Danilo Rodrigues Catapan <dcatapan@gmail.com>
2020-06-11 15:08:25 -0300

    feat: add create challenge dto

---
+++ src/challenge/dtos/create-challenge.dto.ts
@@ -0,0 +1,16 @@
+import { IsNotEmpty, IsDateString, IsArray, ArrayMinSize, ArrayMaxSize } from "class-validator";
+import { Player } from "src/players/interfaces";
+
+export class CreateChallengeDto {
+  @IsNotEmpty()
+  @IsDateString()
+  dateHourChallenge: Date
+
+  @IsNotEmpty()
+  challenger: Player
+
+  @IsArray()
+  @ArrayMinSize(2)
+  @ArrayMaxSize(2)
+  players: Player[]
+}
