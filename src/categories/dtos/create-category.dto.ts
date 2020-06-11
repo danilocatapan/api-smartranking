4123ee1c441f9eaa9abab2d8a8954cad78903787
Danilo Rodrigues Catapan <dcatapan@gmail.com>
2020-06-11 00:51:53 -0300

    refactor: add category dto

---
+++ src/categories/dtos/create-category.dto.ts
@@ -0,0 +1,16 @@
+import { Event } from "../interfaces";
+import { IsString, IsNotEmpty, IsArray, ArrayMinSize } from "class-validator";
+
+export class CreateCategoryDto {
+  @IsString()
+  @IsNotEmpty()
+  readonly category: string
+
+  @IsString()
+  @IsNotEmpty()
+  description: string
+
+  @IsArray()
+  @ArrayMinSize(1)
+  events: Array<Event>
+}
