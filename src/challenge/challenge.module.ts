85236b6ee6771f020e605078afd3876e9e874903
Danilo Rodrigues Catapan <dcatapan@gmail.com>
2020-06-11 16:18:40 -0300

    feat: add schemas, players and categories modules to imports

--- src/challenge/challenge.module.ts
+++ src/challenge/challenge.module.ts
@@ -1,8 +1,17 @@
+import { ChallengeController, ChallengeService } from './';
+import { ChallengeSchema, MatchSchema } from './db';
+import { PlayersModule } from 'src/players';
+import { CategoriesModule } from 'src/categories';
 import { Module } from '@nestjs/common';
-import { ChallengeController } from './challenge.controller';
-import { ChallengeService } from './challenge.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      {name: 'Challenge', schema: ChallengeSchema},
+      {name: 'Match', schema: MatchSchema}]),
+    PlayersModule,
+    CategoriesModule],
   controllers: [ChallengeController],
   providers: [ChallengeService]
 })
