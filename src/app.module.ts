6db77b616a42a1fd8a8230e5a5b0671945962867
Danilo Rodrigues Catapan <dcatapan@gmail.com>
2020-06-11 00:16:30 -0300

    feat: add categories module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common'
 import { MongooseModule } from '@nestjs/mongoose'
 import { PlayersModule } from './players/players.module'
+import { CategoriesModule } from './categories/categories.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot(
       'mongodb+srv://admin:admin123@cluster0-wyrd1.mongodb.net/smartranking?retryWrites=true&w=majority',
       { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: false }),
-    PlayersModule],
+    PlayersModule,
+    CategoriesModule],
   controllers: [],
   providers: [],
 })
