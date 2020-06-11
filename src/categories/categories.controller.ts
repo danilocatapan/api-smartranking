4bba40631def16efc8bc2a1c40c0ffbd8467b997
Danilo Rodrigues Catapan <dcatapan@gmail.com>
2020-06-11 01:16:47 -0300

    feat: add controller save

--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Category } from './interfaces'
+import { CategoriesService } from './categories.service'
+import { CreateCategoryDto } from './dtos/create-category.dto'
+import { Controller, Body, Post, UsePipes, ValidationPipe } from '@nestjs/common'
 
-@Controller('categories')
-export class CategoriesController {}
+@Controller('api/v1/categories')
+export class CategoriesController {
+  constructor (private readonly categoriesService: CategoriesService) {}
+
+  @Post()
+  @UsePipes(ValidationPipe)
+  async save(@Body() createCategoryDto: CreateCategoryDto): Promise<Category> {
+    return await this.categoriesService.save(createCategoryDto)
+  }
+}
