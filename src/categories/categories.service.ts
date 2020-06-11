07a68981b75b3a8425420ac950ab664eabbe7de3
Danilo Rodrigues Catapan <dcatapan@gmail.com>
2020-06-11 01:16:21 -0300

    feat: add service save

--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -1,4 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, BadRequestException } from '@nestjs/common'
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Category } from './interfaces';
+import { CreateCategoryDto } from './dtos/create-category.dto';
 
 @Injectable()
-export class CategoriesService {}
+export class CategoriesService {
+  constructor (@InjectModel('Category') private readonly categoryModel: Model<Category>) {}
+
+  async save(createCategoryDto: CreateCategoryDto): Promise<Category> {
+    const { category } = createCategoryDto
+    const foundCategory = await this.categoryModel.findOne({ category }).exec()
+    if (foundCategory) {
+      throw new BadRequestException(`Category ${category} already registered`)
+    }
+    const newCategory = new this.categoryModel(createCategoryDto)
+    return await newCategory.save()
+  }
+}
