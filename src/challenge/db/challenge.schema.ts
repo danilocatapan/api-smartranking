5c408f0154f603434739bd86cf3263c363bd8a6a
Danilo Rodrigues Catapan <dcatapan@gmail.com>
2020-06-11 16:15:34 -0300

    feat: add challenge schema

---
+++ src/challenge/db/challenge.schema.ts
@@ -0,0 +1,18 @@
+import * as mongoose from 'mongoose';
+
+export const ChallengeSchema = new mongoose.Schema({
+  dateHourChallenge: { type: Date },
+    status: { type: String },
+    dateHoraRequest: { type: Date },
+    DateHoraResponse: { type: Date },
+    challenger: { type: mongoose.Schema.Types.ObjectId, ref: "Player" },
+    category: { type: String },
+    players: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Player"
+    }],
+    match: {
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: "Match" 
+    },    
+}, { timestamps: true, collection: 'challenges' })
