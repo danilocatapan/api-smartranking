9eb84965045119591d4847a59092e4f65da2aac8
Danilo Rodrigues Catapan <dcatapan@gmail.com>
2020-06-11 00:17:28 -0300

    feat: add categories service

--- src/categories/categories.module.ts
+++ src/categories/categories.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { CategoriesController } from './categories.controller';
+import { CategoriesService } from './categories.service';
 
 @Module({
-  controllers: [CategoriesController]
+  controllers: [CategoriesController],
+  providers: [CategoriesService]
 })
 export class CategoriesModule {}
