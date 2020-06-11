adffcf0a5a9692c734c8846b012e2e42cc90f2f9
Danilo Rodrigues Catapan <dcatapan@gmail.com>
2020-06-11 00:51:09 -0300

    refactor: change Event to interface

--- src/categories/interfaces/category.interface.ts
+++ src/categories/interfaces/category.interface.ts
@@ -1,15 +1,10 @@
+import { Event } from "./category.event"
+import { Player } from "src/players/interfaces/"
 import { Document } from "mongoose"
-import { Player } from "src/players/interfaces/player.interface"
 
 export interface Category extends Document {
   readonly category: string
   description: string
   events: Array<Event>
   players: Array<Player>
-}
-
-type Event = {
-  name: string
-  operation: string
-  value: number
 }
