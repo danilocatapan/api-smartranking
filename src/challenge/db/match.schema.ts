c5f87ab2cace51e739857d851fc64578ff72718c
Danilo Rodrigues Catapan <dcatapan@gmail.com>
2020-06-11 16:15:51 -0300

    feat: add match schema

---
+++ src/challenge/db/match.schema.ts
@@ -0,0 +1,15 @@
+import * as mongoose from 'mongoose'
+
+export const MatchSchema = new mongoose.Schema({
+  category: { type: String },
+  players: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Player"
+  }],
+  winner: { 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: "Player" },
+  result: [{
+    set: { type: String }
+  }]
+}, { timestamps: true, collection: 'matches'})
