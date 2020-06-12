ae194a5bfc513bb5c51bed724b9ad000910f2e07
Danilo Rodrigues Catapan <dcatapan@gmail.com>
2020-06-12 00:03:22 -0300

    feat: add update challenge dto

---
+++ src/challenge/dtos/update-challenge.dto.ts
@@ -0,0 +1,11 @@
+import { ChallengeStatus } from "../enums/challenge-status.enum"
+import { IsDateString, IsOptional } from "class-validator"
+
+export class UpdateChallengeDto {
+  @IsOptional()
+  @IsDateString()
+  dateHourChallenge: Date
+
+  @IsOptional()
+  status: ChallengeStatus
+}
