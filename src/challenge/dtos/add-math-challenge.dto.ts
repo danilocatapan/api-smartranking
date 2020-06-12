746b472a300f6f5df4817833bb843db01fc59f98
Danilo Rodrigues Catapan <dcatapan@gmail.com>
2020-06-12 00:29:45 -0300

    feat: add math to challenge dto

---
+++ src/challenge/dtos/add-math-challenge.dto.ts
@@ -0,0 +1,11 @@
+import { Result } from "../interface"
+import { Player } from "src/players/interfaces"
+import { IsNotEmpty } from "class-validator"
+
+export class AddMathChallengeDto {
+  @IsNotEmpty()
+  winner: Player
+
+  @IsNotEmpty()
+  result: Result[]
+}
