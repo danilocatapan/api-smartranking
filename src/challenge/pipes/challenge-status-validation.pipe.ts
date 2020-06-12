2b5b85f62e188f17780226c03b5b430663e572b0
Danilo Rodrigues Catapan <dcatapan@gmail.com>
2020-06-12 00:02:52 -0300

    feat: add challenge status pipe

---
+++ src/challenge/pipes/challenge-status-validation.pipe.ts
@@ -0,0 +1,19 @@
+import { ChallengeStatus } from "../enums/challenge-status.enum"
+import { PipeTransform, BadRequestException } from "@nestjs/common"
+
+export class ChallengeStatusValidationPipe implements PipeTransform {
+  readonly validStatus = [
+    ChallengeStatus.ACCEPT,
+    ChallengeStatus.DENIED,
+    ChallengeStatus.CANCELED
+  ]
+
+  transform(value: any) {
+    const status = value.status.toUpperCase();
+    const valid = this.validStatus.indexOf(status)
+    if (valid === -1) {
+      throw new BadRequestException(`${status} is invalid status`)
+    }
+    return value
+  }
+}
